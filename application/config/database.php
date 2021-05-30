fd911bc4c37462d382595d09780321ad010b7de3
rennuas <as.rennu147@gmail.com>
2021-05-30 13:04:30 +0700

    config for development

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'mysqlserver',
 	'username' => 'root',
-	'password' => '',
-	'database' => 'posr',
+	'password' => 'tiger',
+	'database' => 'pos',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
